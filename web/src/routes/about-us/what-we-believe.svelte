adb4d063cdb86103620c2c67e975f9266506ffb3
Robert Allmond <ballmond@gmail.com>
2020-12-10 15:32:13 -0500

    initial navbar impl

---
+++ web/src/routes/about-us/what-we-believe.svelte
@@ -0,0 +1,114 @@
+<style>
+
+	h1,
+	h2,
+	p {
+		font-family: system-ui, -apple-system, sans-serif;
+	}
+	h1 {
+		font-size: 2.666666666667em;
+		font-weight: 700;
+		margin: 0 0 0.5em;
+		text-transform: uppercase;
+		margin-top: 0.5em;
+  		line-height: 1.1;
+	}
+	h1:first-child {
+  		border-bottom: 2px dotted #666;
+	}
+	h2 {
+		font-size: 2.222222222222em; /* 40px /18 */
+		border-bottom: 1px solid #ddd;
+		margin: 1em 0 0.25em;
+		line-height: 1.1;
+		text-transform: uppercase;
+	}
+	p {
+  		/* max-width: 42em; */
+  		line-height: 1.5;
+	}
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		max-width: 100%;
+		margin: 1rem auto;
+		padding: 1rem;
+		font-size: 1.125em;
+	}
+	iframe {
+  		border: none;
+  		width: 100%;
+	}
+	.container {
+		background: var(--grey);
+		/* margin-bottom: .25rem; */
+	}
+	.content {
+		max-width: 76rem;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	/* .container:nth-child(even) {
+		background: var(--grey);
+	} */
+</style>
+
+<div class="container">
+    <div class="content">
+<h1>What We Believe</h1>
+<p>
+    We believe that God, who created the universe in the beginning, is still actively creating.
+
+    We believe that faith in Jesus Christ puts us in a new and vital relationship with God.
+    
+    We believe that the Holy Spirit is present and active among those who believe,  encouraging us to love one another and to do good works in the world.
+    
+    We believe that the best way to find out what God is like  is to study the teachings of Jesus and to understand the attitudes and convictions which guided his life.
+    
+    We believe that God loves and cares about people of every race,  language, and nation on the face of the earth.
+    
+    We believe that “all have sinned and fall short of the glory of God”,  but that God loves every child and every man and woman on earth,  and seeks to bring every person into spiritual maturity and into perfect love.
+    
+    We believe that worship is for the praise of God and for celebrating God’s love together,  and that worship demands, whatever excellence in music,  preaching, prayer, and the use of all our gifts, which we can bring to it.
+</p>
+<h3><strong><em>We believe our mission is to serve others and to reach out to share with them the joy of the Gospel and all that Christ means to us.</em></strong></h3>
+<p>We believe in YOU, your gifts, and your potential.</p>
+<h3><strong>Our Vision:</strong></h3>
+<p>
+    <strong>B</strong>ecoming a Missional Church<br>
+    <strong>E</strong>ncouraging all participants to grow spiritually<br>
+    <strong>C</strong>aring for the needs of our church family<br>
+    <strong>O</strong>rganizing for g<br>
+    <strong>M</strong>otivating all participants to serve the community and the world<br>
+    <strong>E</strong>nabling first-time visitors to become active in the life and ministry of Grace<br>
+</p>
+<h3><strong>Ten Facts You Should Know About American Baptists</strong></h3>
+<p>
+    The 1.3-million members and 5,200 congregations of American Baptist Churches USA share with more than 42 million Baptists around the world a common tradition begun in the early 17th century. That tradition has emphasized the Lordship and atoning sacrifice of Jesus Christ, believers’ baptism, the competency of all believers to be in direct relationship with God and to interpret Scripture, the importance of the local church, the assurance of freedom in worship and opinion, and the need to be Christ’s witnesses within society.
+    <br>
+    <br>
+    The following facts are representative of the tradition and practice of American Baptists.
+</p>
+<ul>
+    <li>American Baptists believe that Jesus Christ is Lord and Savior, and that the Bible is the divinely inspired word of God that serves as the final written authority for living out the Christian faith.</li>
+    <li>For American Baptists the local church is the fundamental unit of mission in denominational life.</li>
+    <li>American Baptists partake of two ordinances: believers’ baptism and The Lord’s Supper.</li>
+    <li>American Baptists believe that the committed individual Christian can and should approach God directly, and that individual gifts of ministry should be shared.</li>
+    <li>American Baptists take seriously the call to evangelism and missionary work.</li>
+    <li>American Baptists support religious freedom and respect the expressions of faith of others.</li>
+    <li>American Baptists acknowledge that God’s family extends beyond our local churches, and that God calls us to  cooperative  ministries.</li>
+    <li>American Baptists have been called to be Christ’s witnesses for justice and wholeness within a broken society.</li>
+    <li>American Baptist Churches USA celebrates the racial, cultural and theological diversity witnessed within its membership.</li>
+    <li>American Baptists heed the biblical call to renewal and the need for a vital witness in a new millennium.</li>
+</ul>
+<h3><strong><em>Facts you should know about Grace Baptist Church</em></strong></h3>
+<ul>
+    <li>Grace Baptist Church, founded in 1872, is an American Baptist Church, and currently, we are members of the Philadelphia Baptist Association (PBA), which is the oldest continuous association of Baptist churches in the United States.</li>
+    <li>Our congregation was originally located in Philadelphia. Its original name was also Grace Baptist Church. When the church started to grow, and they erected a new church building at Broad and Berks Streets, it took on the nickname “The Temple.”</li>
+    <li>Hattie May Wiatt was a small girl who saved up 57 cents for the building of the new church facility. She sadly passed away in 1886       before the land was purchased, but Dr. Conwell used her donation to help raise the other necessary funds. The full story is on our website- gracebaptistbluebell.org. It has been retold for many years in many congregations across the country.</li>
+    <li>Dr. Russell Conwell, who was pastor of the church at the time, oversaw the building of the new, much larger church building, and also facilitated the founding of The Temple University (which started with 7 individuals who sought a higher education, but did not have the money to pay for it), as well as the founding of Temple Hospital (the small Samaritan, Garretson, and Greatheart hospitals were merged to form one larger hospital).</li>
+    <li>The congregation moved out to Blue Bell in 1975.</li>
+</ul>
+</div>
+</div>
