eee0b5bbf5afb66f38d65b4e179b1f574d724a61
Robert Allmond <ballmond@gmail.com>
2020-12-22 09:11:20 -0500

    updated sanity/cli package

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -41,6 +41,9 @@
 	const siteSettings = getContext('siteSettings')
 	export let posts;
 
+	let subscribe = "Subscribe";
+	let mc_success = "";
+	let mc_error = "";
 	let email = "";
 	let fname = "";
 	let lname = "";
@@ -61,6 +64,9 @@
 		},
 			body: JSON.stringify(user)
 		}).then((x) => x.json());
+		console.log(JSON.stringify(response, null, 2))
+		subscribe = "Subscribed!"
+		mc_success = "LOL"
 	}
 </script>
 	
@@ -134,7 +140,45 @@
 		width: 90%;
 		margin: 0 5%;
 		clear: both;
-	}	
+	}
+	#mc_embed_signup .mc-field-group input {
+		display: block;
+		width: 100%;
+		padding: 8px 0;
+		text-indent: 2%;
+	}
+	#mc_embed_signup input {
+		border: 1px solid #ABB0B2;
+		-webkit-border-radius: 3px;
+		-moz-border-radius: 3px;
+		border-radius: 3px;
+	}
+	#mc_embed_signup .indicates-required {
+		text-align: right;
+		font-size: 11px;
+		margin-right: 4%;
+	}
+	#mc_embed_signup .button {
+		clear: both;
+		background-color: #aaa;
+		border: 0 none;
+		border-radius: 4px;
+		transition: all 0.23s ease-in-out 0s;
+		color: #FFFFFF;
+		cursor: pointer;
+		display: inline-block;
+		font-size: 15px;
+		font-weight: normal;
+		height: 32px;
+		line-height: 32px;
+		margin: 0 5px 10px 0;
+		padding: 0 22px;
+		text-align: center;
+		text-decoration: none;
+		vertical-align: top;
+		white-space: nowrap;
+		width: auto;
+	}
 </style>
 
 <div class="wrapper">
@@ -199,7 +243,29 @@
 
 	<div class="container">
 		<div class="content">
-			<h2>Newsletter</h2>
+			<h2>Subscribe</h2>
+			<div id="mc_embed_signup">
+				<h3>Stay current with all things happening at Grace Baptist Church of Blue Bell</h3>
+				<form on:click|preventDefault id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form">
+					<div class="mc-field-group">
+					  <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+					  <input bind:value={email} type="email" name="EMAIL" class="required email" id="mce-EMAIL">
+					</div>
+				    <div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"><span><p>{mc_error}</p></span></div>
+						<div class="response" id="mce-success-response" style="display:none"><span><p>{mc_success}</p></span></div>
+					</div>
+					<div class="clear" {subscribe}>
+						<input on:click={onClick} type="submit" bind:value={subscribe} name="subscribe" id="mc-embedded-subscribe" class="button">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="content">
+			<h2>Subscribe to our newsletter!</h2>
 			<!-- Begin Mailchimp Signup Form -->
 			<!--<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">-->
 			<!--
@@ -213,27 +279,27 @@
 			<!--<form action="https://gmail.us7.list-manage.com/subscribe/post?u=9ba083eded6d2e2dc934dd2aa&amp;id=c6ace2212d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>-->
 			<form  on:click|preventDefault id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate>
 				<div id="mc_embed_signup_scroll">
-				<h2>Subscribe</h2>
-			<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-			<div class="mc-field-group">
-				<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-			</label>
-				<input bind:value={email} type="email" name="EMAIL" class="required email" id="mce-EMAIL">
-			</div>
-			<div class="mc-field-group">
-				<label for="mce-FNAME">First Name </label>
-				<input type="text" bind:value={fname} name="FNAME" class="" id="mce-FNAME">
-			</div>
-			<div class="mc-field-group">
-				<label for="mce-LNAME">Last Name </label>
-				<input type="text"  bind:value={lname} name="LNAME" class="" id="mce-LNAME">
-			</div>
-				<div id="mce-responses" class="clear">
-					<div class="response" id="mce-error-response" style="display:none"></div>
-					<div class="response" id="mce-success-response" style="display:none"></div>
-				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-				<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9ba083eded6d2e2dc934dd2aa_c6ace2212d" tabindex="-1" value=""></div>
-				<div class="clear"><input on:click={onClick} type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+				  <h3>Stay current with all things happening at Grace Baptist Church of Blue Bell</h3>
+				    <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+				    <div class="mc-field-group">
+				      <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+			          </label>
+				      <input bind:value={email} type="email" name="EMAIL" class="required email" id="mce-EMAIL">
+			        </div>
+			        <div class="mc-field-group">
+				      <label for="mce-FNAME">First Name </label>
+				      <input type="text" bind:value={fname} name="FNAME" class="" id="mce-FNAME">
+			        </div>
+			        <div class="mc-field-group">
+				      <label for="mce-LNAME">Last Name </label>
+				      <input type="text"  bind:value={lname} name="LNAME" class="" id="mce-LNAME">
+			        </div>
+				    <div id="mce-responses" class="clear">
+					  <div class="response" id="mce-error-response" style="display:none"></div>
+					  <div class="response" id="mce-success-response" style="display:none"></div>
+				    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9ba083eded6d2e2dc934dd2aa_c6ace2212d" tabindex="-1" value=""></div>
+				    <div class="clear"><input on:click={onClick} type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 				</div>
 			</form>
 			</div>
